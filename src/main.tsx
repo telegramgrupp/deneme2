fbe1d85449031992d04f8426a32376c97c30ab8f
telegramgrup.com <muratctlll@gmail.com>
2025-05-11 16:44:33 +0300

    Setup React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App.tsx';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import App from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <ToastContainer position="top-right" autoClose={3000} />
     </BrowserRouter>
   </StrictMode>
 );
